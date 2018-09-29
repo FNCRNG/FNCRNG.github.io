dad98068dcf04a2277a858c033b46c7ea6d010e1
FNCRNG <43704967+FNCRNG@users.noreply.github.com>
2018-09-29 13:41:19 -0400

    1

---
+++ imgReferences.js
@@ -0,0 +1,29 @@
+function load() {
+	/*Types*/
+		document.getElementById('typeBug').src = "https://serebii.net/pokedex-bw/type/bug.gif";
+		document.getElementById('typeDark').src = "https://serebii.net/pokedex-bw/type/dark.gif";
+		document.getElementById('typeDrag').src = "https://serebii.net/pokedex-bw/type/dragon.gif";
+		document.getElementById('typeElct').src = "https://serebii.net/pokedex-bw/type/electric.gif";
+		document.getElementById('typeFight').src = "https://serebii.net/pokedex-bw/type/fighting.gif";
+		document.getElementById('typeFire').src = "https://serebii.net/pokedex-bw/type/fire.gif";
+		document.getElementById('typeFly').src = "https://serebii.net/pokedex-bw/type/flying.gif";
+		document.getElementById('typeGhost').src = "https://serebii.net/pokedex-bw/type/ghost.gif";
+		document.getElementById('typeGrass').src = "https://serebii.net/pokedex-bw/type/grass.gif";
+		document.getElementById('typeGround').src = "https://serebii.net/pokedex-bw/type/ground.gif";
+		document.getElementById('typeIce').src = "https://serebii.net/pokedex-bw/type/ice.gif";
+		document.getElementById('typeNorm').src = "https://serebii.net/pokedex-bw/type/normal.gif";
+		document.getElementById('typePois').src = "https://serebii.net/pokedex-bw/type/poison.gif";
+		document.getElementById('typePsy').src = "https://serebii.net/pokedex-bw/type/psychic.gif";
+		document.getElementById('typeRock').src = "https://serebii.net/pokedex-bw/type/rock.gif";
+		document.getElementById('typeSteel').src = "https://serebii.net/pokedex-bw/type/steel.gif";
+		document.getElementById('typeWater').src = "https://serebii.net/pokedex-bw/type/water.gif";
+
+	/*Platinum Pokemon Sprites*/	
+/*152*/	document.getElementById('pChikorita').src = "https://www.serebii.net/pokearth/sprites/dp/152.png";
+/*155*/	document.getElementById('pCyndaquil').src = "https://www.serebii.net/pokearth/sprites/dp/155.png";
+/*158*/	document.getElementById('pTotodile').src = "https://www.serebii.net/pokearth/sprites/dp/158.png";
+/*290*/	document.getElementById('pNincada').src = "https://www.serebii.net/pokearth/sprites/dp/290.png";
+/*300*/	document.getElementById('pSkitty').src = "https://www.serebii.net/pokearth/sprites/dp/300.png";
+/*349*/	document.getElementById('pFeebas').src = "https://www.serebii.net/pokearth/sprites/dp/349.png";
+//*num*/		document.getElementById('name').src = "https:";
+}
